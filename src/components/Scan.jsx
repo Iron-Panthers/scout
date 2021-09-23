43870378711afdaea6c9eb38df5832c6cf1a153f
Isaac <isaac@rubenfamily.com>
2021-09-23 10:51:24 -0700

    add qr code

--- src/components/Scan.jsx
+++ src/components/Scan.jsx
@@ -1,9 +1,19 @@
-import React from "react"
+import React, { useContext, useMemo } from "react"
 import Reset from "./inputs/Reset"
+import QRCode from "react-qr-code";
 
+import { Context } from "../state";
+
+import "./inputs/QRCode.scss";
 
 const Scan = () => {
+  const [state, dispatch] = useContext(Context)
+  const value = useMemo(
+    () => JSON.stringify(state),
+    [state]
+  )
   return <>
+    <div className="qr"><QRCode value={JSON.stringify(state)} size={350}></QRCode></div>
     <Reset></Reset>
   </>
 }
