163e5de03752f2c20e853a88f5611210e34406a3
Isaac <isaac@rubenfamily.com>
2021-09-23 16:32:34 -0700

    keep track of scanning

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 
 import Reset from "./inputs/Reset"
 import QrReader from "react-qr-reader"
@@ -8,14 +8,18 @@ import "./inputs/inputs.scss"
 
 const Review = () => {
   const [error, setError] = useState(false)
-
+  const scans = useRef(new Set())
+  const [scanCount, setScanCount] = useState(0)
 
   return <>
-    <div className="wide tall QrWrapper">
+    <div className="QrWrapper">
       <QrReader
         onScan={val => {
           if (val === null) return
-          console.log(val)
+          if (!scans.current.has(val)) {
+            scans.current.add(val)
+            setScanCount(scanCount + 1)
+          }
         }}
         onError={err => {
           console.error(err)
@@ -23,6 +27,7 @@ const Review = () => {
         }}
       ></QrReader>
     </div>
+    <div className="Center">{`Scanned ${scanCount}`}</div>
     {error && <div className="wide Center">{error}</div>}
     <Reset></Reset>
   </>
