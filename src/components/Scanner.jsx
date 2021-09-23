f97ce2318af37e12d0fb460673104a88f2f1d9cf
Isaac <isaac@rubenfamily.com>
2021-09-23 16:15:53 -0700

    working qr code scanner

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 
 import Reset from "./inputs/Reset"
+import QrReader from "react-qr-reader"
 
+import "./Scanner.scss"
 
 const Review = () => {
+  const [error, setError] = useState(false)
+
+
   return <>
+    <div className="wide tall QrWrapper">
+      <QrReader
+        onScan={val => {
+          if (val === null) return
+          console.log(val)
+        }}
+        onError={err => {
+          console.error(err)
+        }}
+      ></QrReader>
+    </div>
     <Reset></Reset>
   </>
 }
