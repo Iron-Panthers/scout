2a6c18183595b80ec699831c43474bd63cf372e7
Isaac <isaac@rubenfamily.com>
2021-09-24 11:41:35 -0700

    add reset functionality

--- src/components/Export.jsx
+++ src/components/Export.jsx
@@ -1,14 +1,31 @@
-import React, { useRef, useState } from "react"
+import React, { useRef, useState, useContext } from "react"
+
+import { Context } from "../state";
 
 import Reset from "./inputs/Reset"
 
 import "./inputs/inputs.scss"
 
 const Export = () => {
+  const [state, dispatch] = useContext(Context)
+
   const scans = JSON.parse(localStorage.scanSet ?? '[]').map(str => JSON.parse(str))
 
   return <>
-    <div className="Center">{`Scanned ${scans.length}`}</div>
+    <div className="Center wide">{`Scanned and Stored ${scans.length} QR${scans.length > 1 ? "s" : ""}`}</div>
+    <button
+    disabled={scans.length === 0}
+    onClick={() => {
+      if(window.confirm("Are you very, very sure? All scanned QR code data will be lost forever.")) {
+        delete localStorage.scanSet
+        alert("Cleared, resetting app state.")
+        dispatch({type: "reset"})
+
+      } else {
+        alert("No data was cleared.")
+      }
+    }}
+    >Tap to CLEAR ALL {scans.length} STORED QR CODES PERMANENTLY</button>
     <Reset></Reset>
   </>
 }
