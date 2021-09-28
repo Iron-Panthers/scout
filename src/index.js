5daac57f028e285a8997530bc7d99b318d30ae7c
Isaac <isaac@rubenfamily.com>
2021-09-28 15:49:03 -0700

    prevent reload

--- src/index.js
+++ src/index.js
@@ -17,6 +17,16 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
+
+// prevent reload
+window.addEventListener('beforeunload', function (e) {
+  // Cancel the event
+  e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+  // Chrome requires returnValue to be set
+  e.returnValue = '';
+});
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
