ffddbe5512ff3285fd8890dab17970b52a0a3398
Isaac <isaac@rubenfamily.com>
2021-10-02 23:02:29 -0700

    use stupid complexity closures to make undo work as intended

--- src/components/inputs/Switch.jsx
+++ src/components/inputs/Switch.jsx
@@ -11,14 +11,16 @@ const Switch = ({ options: {
   return <>
     <Bool {...{
       phase, onFlip: undo => {
-        dispatch({ type: `set${phase ? "InPhase" : ""}`, prop: opB.prop, val: false })
-        if(onFlip !== undefined) onFlip(undo)
+        const otherVal = phase ? (state[state.phase] ?? {})[opB.prop] : state[opB.prop]
+        dispatch({ type: `set${phase ? "InPhase" : ""}`, prop: opB.prop, val: !undo ? false : otherVal, undo: true })
+        if (onFlip !== undefined) onFlip(undo)
       }, ...opA
     }}></Bool>
     <Bool {...{
       phase, onFlip: undo => {
-        dispatch({ type: `set${phase ? "InPhase" : ""}`, prop: opA.prop, val: false })
-        if(onFlip !== undefined) onFlip(undo)
+        const otherVal = phase ? (state[state.phase] ?? {})[opB.prop] : state[opB.prop]
+        dispatch({ type: `set${phase ? "InPhase" : ""}`, prop: opA.prop, val: !undo ? false : otherVal, undo: true })
+        if (onFlip !== undefined) onFlip(undo)
       }, ...opB
     }}></Bool>
   </>
