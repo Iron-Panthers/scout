d198f8d0b5b1c3dd602dcef1685261e56884896f
Isaac <isaac@rubenfamily.com>
2022-01-18 15:09:02 -0800

    port to hub

--- src/components/inputs/Shoot.jsx
+++ src/components/inputs/Shoot.jsx
@@ -3,9 +3,9 @@ import Count from "./Count"
 
 const Shoot = () => (
   <>
-    <Count phase label="Upper port" prop="upperSucc" color="green"></Count>
+    <Count phase label="Upper hub" prop="upperSucc" color="green"></Count>
     <Count phase label="fail" prop="upperFail" color="red"></Count>
-    <Count phase label="Lower port" prop="lowerSucc" color="green"></Count>
+    <Count phase label="Lower hub" prop="lowerSucc" color="green"></Count>
     <Count phase label="fail" prop="lowerFail" color="red"></Count>
   </>
 )
