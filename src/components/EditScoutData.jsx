84d79cce08ceaf0c684a690936f0de7cedddaa3a
Isaac <isaac@rubenfamily.com>
2022-04-10 01:10:59 -0700

    let nested data be hidden

--- src/components/EditScoutData.jsx
+++ src/components/EditScoutData.jsx
@@ -1,6 +1,6 @@
 import SetPanel from "./inputs/SetPanel"
 import { Context } from "../state"
-import { useContext, useMemo } from "react"
+import React, { useContext, useMemo, useState } from "react"
 import { filterState } from "../csv"
 
 import "./EditScoutData.scss"
@@ -30,20 +30,31 @@ const DataButton = ({ label, value, disabled, fn }) => {
     return button(value.toString(), elementTypes.Boolean)
   else if (value === null) return button("null", elementTypes.Null)
   else if (value === undefined)
-    return button("undefined!", elementTypes.Undefined)
+    return button("undefined", elementTypes.Undefined)
   return button(`"${value}"`, elementTypes.String)
 }
+
+const LabelGroup = React.memo(({ label, obj }) => {
+  const [hidden, setHidden] = useState(false)
+
+  return (
+    <div className={`labelGroup${hidden ? " hidden" : ""}`}>
+      <button
+        className="Object label"
+        onClick={() => {
+          setHidden((previousVal) => !previousVal)
+        }}
+      >{`${label}: (tap to ${hidden ? "expand" : "collapse"})`}</button>
+      {objectVisualizer(obj)}
+    </div>
+  )
+})
 
 const objectVisualizer = (obj) =>
   Object.entries(obj).map(([key, value]) => {
     if (Object(value) === value) {
       // is an object, we need to go deeper
-      return (
-        <div className="labelGroup" key={key}>
-          <button className="Object label">{`${key}: `}</button>
-          {objectVisualizer(value)}
-        </div>
-      )
+      return <LabelGroup obj={value} label={key} key={key} />
     }
 
     return (
@@ -63,8 +74,6 @@ const EditScoutData = () => {
   // console.log(state)
 
   const data = useMemo(() => objectVisualizer(filterState(state)), [state])
-
-  console.log(data)
 
   return (
     <>
