801d871340122b2dc0727ffd11a8254a089aab1e
Isaac <isaac@rubenfamily.com>
2022-04-10 17:33:13 -0700

    unit test csv output from stateToCsv

--- src/csv.test.js
+++ src/csv.test.js
@@ -1,6 +1,10 @@
-import { filterState } from "./csv"
+import { filterState, stateToCsv } from "./csv"
 import { initialState } from "./reducer"
 
 it("matches snapshot for filter on initialState", () => {
   expect(JSON.stringify(filterState(initialState))).toMatchSnapshot()
+})
+
+it("matches snapshot for state to csv on initialState", () => {
+  expect(JSON.stringify(stateToCsv(initialState))).toMatchSnapshot()
 })
