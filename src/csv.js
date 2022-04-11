3b00bca37f155f1798afa180f10bd615430a69a9
Isaac <isaac@rubenfamily.com>
2022-04-10 18:46:04 -0700

    add comments, snapshots

--- src/csv.js
+++ src/csv.js
@@ -1,8 +1,10 @@
 import { unparse, parse } from "papaparse"
 import { initialState } from "./reducer"
 
+/** remove unwanted keys from state */
 export const filterState = ({ mode, phase, undoStack, ...state }) => state
 
+/** flattens, cleans a state object such that its keys are hyphen separated of their parents*/
 export const cleanState = (rawState) =>
   Object.entries(filterState(rawState)).reduce((obj, [key, val]) => {
     if (typeof val === "object") {
@@ -15,12 +17,15 @@ export const cleanState = (rawState) =>
     return obj
   }, {})
 
+/** the fields on a state - the flattened keys */
 export const fields = Object.entries(cleanState(initialState)).map(
   ([key]) => key
 )
 
+/** the csv header for a csv ed state */
 export const header = unparse({ fields })
 
+/** converts state into a csv string */
 export const stateToCsv = (state) =>
   unparse(
     {
@@ -30,6 +35,7 @@ export const stateToCsv = (state) =>
     { header: false }
   )
 
+/** converts the body of a csv string into a flattened state object */
 export const parseCsvBody = (body) => {
   const obj = parse(`${header}\r\n${body}`)
   const headArray = obj.data[0]
@@ -43,4 +49,18 @@ export const parseCsvBody = (body) => {
   })
 
   return returnObj
+}
+
+export const castType = (string) => {
+  if (Number.isFinite(string)) return Number.parseFloat(string)
+  if ((string === "true") | (string === "false"))
+    return string === "true" ? true : false
+  return string
+}
+
+/** shallow iteration over an object, to cast types into their respective values if applicable or leave them as strings */
+export const castTypes = (object) => {
+  return Object.entries(object).reduce((obj, [key, value]) => {
+    return (obj[key] = castType(value))
+  }, {})
 }
--- src/csv.test.js
+++ src/csv.test.js
@@ -1,10 +1,30 @@
-import { filterState, stateToCsv } from "./csv"
+import {
+  filterState,
+  stateToCsv,
+  cleanState,
+  parseCsvBody,
+  castTypes,
+} from "./csv"
 import { initialState } from "./reducer"
 
 it("matches snapshot for filter on initialState", () => {
   expect(JSON.stringify(filterState(initialState))).toMatchSnapshot()
 })
 
+it("matches snapshot for cleanState on initialState", () => {
+  expect(JSON.stringify(cleanState(initialState))).toMatchSnapshot()
+})
+
 it("matches snapshot for state to csv on initialState", () => {
   expect(JSON.stringify(stateToCsv(initialState))).toMatchSnapshot()
 })
+
+// it("produces the same object from parseCsvBody as stateToCsv made", () => {
+//   const state = { ...initialState }
+//   // csv is the body of our state, after being cleaned, flattened
+//   const csv = stateToCsv(state)
+//   // parsed data is the object generated from reading that body back into an object after attaching a header
+//   const parsedData = parseCsvBody(csv)
+
+//   expect(castTypes(parsedData)).toEqual(cleanState(state))
+// })
