083ba7487759ecc6f8e03050e301ce7a4e6f177f
Isaac <isaac@rubenfamily.com>
2022-04-30 18:27:43 -0700

    make sure every panel can render without crashing

--- src/components/Panels.test.js
+++ src/components/Panels.test.js
@@ -2,15 +2,40 @@ import Panels from "./Panels"
 import React from "react"
 import { screen } from "@testing-library/react"
 
-it("renders without crashing", () => {
-  renderContext(<Panels></Panels>)
+import { initialState } from "../reducer.js"
+
+describe("panel component", () => {
+  it("renders without crashing", () => {
+    renderContext(<Panels></Panels>)
+  })
+
+  it("advances panel when next is clicked", () => {
+    renderContext(<Panels></Panels>)
+    expect(screen.getByText("NEXT")).toBeInTheDocument()
+    screen.getByText("NEXT").click()
+    expect(screen.queryByText("NEXT")).not.toBeInTheDocument()
+  })
 })
 
-it("advances panel when next is clicked", () => {
-  renderContext(<Panels></Panels>)
-  expect(screen.getByText("NEXT")).toBeInTheDocument()
-  screen.getByText("NEXT").click()
-  expect(screen.queryByText("NEXT")).not.toBeInTheDocument()
+describe("child panels", () => {
+  ;[
+    "Configure",
+    "Scout",
+    "Review",
+    "EditScoutData",
+    "ScanData",
+    // "Scanner",
+    // scanner cannot be tested because its reader component doesn't play nice with the virtual environment
+    "Export",
+    "Settings",
+  ].forEach((panel) => {
+    describe(`${panel} panel`, () => {
+      // this will be run for every panel in the array
+      it("renders without crashing", () => {
+        renderContext(<Panels></Panels>, { ...initialState, mode: panel })
+      })
+    })
+  })
 })
 
 it("matches snapshot", () => {
