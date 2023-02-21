97fa6fef17be52664ff68f4854683386ab1c8b78
Brandon <25nguyenb26@stu.smuhsd.org>
2023-02-20 16:53:44 -0800

    Only one scanner

--- src/components/ConfigureQualitative.jsx
+++ src/components/ConfigureQualitative.jsx
@@ -35,7 +35,7 @@ const ConfigureQualitative = () => {
         prop="matchType"
         options={["Test", "Practice", "Qualification"]}
       ></Dropdown>
-      {!isIOS && <SetPanel width = "halfWide" label="Qualitative Scanner" panelName="qualitativeScanner"></SetPanel>}
+      {!isIOS && <SetPanel width = "halfWide" label="Scanner" panelName="Scanner"></SetPanel>}
       <Next width ={isIOS ? "wide" : "halfWide"} qualitative></Next>
     </>
   )
