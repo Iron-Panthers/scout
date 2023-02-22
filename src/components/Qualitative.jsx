16e537215f190b17b6e38a7875528bea315c98b2
Brandon <25nguyenb26@stu.smuhsd.org>
2023-02-22 12:07:10 -0800

    Team checking
    Making sure that the teams have different values before allowing the user to progress

--- src/components/Qualitative.jsx
+++ src/components/Qualitative.jsx
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useContext } from "react"
+
+import { Context } from "../state"
 
 import Next from "./inputs/Next"
 import Info from "./inputs/Info"
 import QualitativeTeam from "./inputs/QualitativeTeam"
 
 const Qualitative = () => {
+  const [state] = useContext(Context)
+
+  const ensureDifferentValues = (team1, team2, team3) => {
+    return team1 !== team2 && team1 !== team3 && team2 !== team3
+  }
+
+  const allDifferentValues =  ensureDifferentValues(state.team1Quickness, state.team2Quickness, state.team3Quickness) &&
+                              ensureDifferentValues(state.team1FieldAwareness, state.team2FieldAwareness, state.team3FieldAwareness)
 
   return (
     <>
