b47b5011e9402332b3d6c73309232a897362eca0
Brandon <25nguyenb26@stu.smuhsd.org>
2023-02-23 08:28:24 -0800

    Getting rid of different value checking

--- src/components/Qualitative.jsx
+++ src/components/Qualitative.jsx
@@ -9,12 +9,12 @@ import QualitativeTeam from "./inputs/QualitativeTeam"
 const Qualitative = () => {
   const [state] = useContext(Context)
 
-  const ensureDifferentValues = (team1, team2, team3) => {
-    return team1 !== team2 && team1 !== team3 && team2 !== team3
-  }
+  // const ensureDifferentValues = (team1, team2, team3) => {
+  //   return team1 !== team2 && team1 !== team3 && team2 !== team3
+  // }
 
-  const allDifferentValues =  ensureDifferentValues(state.team1Quickness, state.team2Quickness, state.team3Quickness) &&
-                              ensureDifferentValues(state.team1FieldAwareness, state.team2FieldAwareness, state.team3FieldAwareness)
+  // const allDifferentValues =  ensureDifferentValues(state.team1Quickness, state.team2Quickness, state.team3Quickness) &&
+  //                             ensureDifferentValues(state.team1FieldAwareness, state.team2FieldAwareness, state.team3FieldAwareness)
 
   return (
     <>
@@ -24,7 +24,8 @@ const Qualitative = () => {
       <QualitativeTeam team = "team1"/>
       <QualitativeTeam team = "team2"/>
       <QualitativeTeam team = "team3"/>
-     <Next width ="wide" tall qualitative = {true} isDisabled = {!allDifferentValues} disabledText="All teams must have different value"></Next>
+     {/* <Next width ="wide" tall qualitative = {true} isDisabled = {!allDifferentValues} disabledText="All teams must have different value"></Next> */}
+     <Next width ="wide" tall qualitative = {true} />
     </>
   )
 }
